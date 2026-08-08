9b91b631a36b96304ffc4b7866e951b6a3c63566
D-vyesh <vaghdivyesh007@gmail.com>
2026-08-08 19:34:33 +0530

    fix: increase kafka connection timeouts

--- capitalup-backend/src/kafka/kafka.js
+++ capitalup-backend/src/kafka/kafka.js
@@ -3,6 +3,14 @@ const { Kafka } = require("kafkajs");
 const kafka = new Kafka({
     clientId: "capitalup-backend",
     brokers: process.env.KAFKA_BROKERS.split(","),
+    connectionTimeout: 10000,
+    authenticationTimeout: 10000,
+    ssl: true,
+    sasl: {
+        mechanism: "scram-sha-256",
+        username: process.env.KAFKA_SASL_USERNAME,
+        password: process.env.KAFKA_SASL_PASSWORD,
+    },
 });
 
 module.exports = kafka;
